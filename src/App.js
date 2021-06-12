9ca1f6fc873b7cbcb547459a9e54f0fbfc8dd913
Rex Osariemen <rexosariemen@gmail.com>
2021-06-12 11:03:48 -0400

    complete excercise - 7mins

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Play from './Interview';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
        <h1>WorkBench for Interviews</h1>
       <header className="App-header">
         <h3>Place your component below here</h3>
+        <Play />
       </header>
     </div>
   );
